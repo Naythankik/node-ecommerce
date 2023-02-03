2aa946943d414db77c19bbc92180d5aacb9c7e32
naythankik <dolapokiki3ird@gmail.com>
2023-02-03 08:34:41 +0100

    Authentication is working perfectly

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,13 +1,13 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const { generateToken } = require("../config/jwtToken");
+const validateMongoDbId = require("../utils/validateMongoDbId");
 
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email });
   if (!findUser) {
     //create a new user
-
     const newUser = await User.create(req.body);
     res.json({
       message: "User has been created successfully",
@@ -42,4 +42,110 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createUser, loginUser };
+const getAllUser = asyncHandler(async (req, res) => {
+  const getUsers = await User.find();
+  try {
+    res.json(getUsers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const findUser = await User.findById(id);
+    res.json(findUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteAuser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deleteAUser = await User.findByIdAndDelete(id);
+    res.json(deleteAUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateAUser = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  validateMongoDbId(id);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        firstname: req?.body?.firstname,
+        lastname: req?.body?.lastname,
+        email: req?.body?.email,
+        mobile: req?.body?.mobile,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json(updatedUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const blockUser = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  validateMongoDbId(id);
+  try {
+    const blocked = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: true,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({
+      message: "User blocked",
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const unBlockUser = asyncHandler(async (req, res) => {
+  const { id } = req.user;
+  validateMongoDbId(id);
+  try {
+    const unblocked = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: false,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json(
+      res.json({
+        message: "User Unblocked",
+      })
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = {
+  createUser,
+  loginUser,
+  getAllUser,
+  getAUser,
+  deleteAuser,
+  updateAUser,
+  blockUser,
+  unBlockUser,
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const { createUser, loginUser } = require("../controller/userCtrl");
-
+const {
+  createUser,
+  loginUser,
+  getAllUser,
+  getAUser,
+  deleteAuser,
+  updateAUser,
+  blockUser,
+  unBlockUser,
+} = require("../controller/userCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
+router.get("/all-users", getAllUser);
+router.get("/:id", authMiddleware, isAdmin, getAUser);
+router.delete("/:id", deleteAuser);
+router.put("/edit-user", authMiddleware, updateAUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, unBlockUser);
 module.exports = router;
